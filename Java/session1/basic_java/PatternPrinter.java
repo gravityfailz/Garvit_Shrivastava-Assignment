5fd635ef394f601442853b62d2412263bce763af
gravityfailz <garvitjishrivastava10@gmail.com>
2026-04-08 14:07:13 +0530

    added pattern printer in basic java folder

---
+++ Java/session1/basic_java/PatternPrinter.java
@@ -0,0 +1,20 @@
+package session1.basic_java;
+
+import java.util.*;
+
+public class PatternPrinter {
+
+    public void printTriangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        PatternPrinter printer = new PatternPrinter();
+        printer.printTriangle(5);
+    }
+}
