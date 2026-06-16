ddfced86166c4c840ab4fa0354731de3d0eca6c9
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-16 16:37:23 +0530

    feat: add python version checker

---
+++ Garvit_Shrivastava_python_training/basic_python/introduction/introduction.py
@@ -0,0 +1,12 @@
+"""
+Question 1
+"""
+
+
+def print_welcome_message() -> None:
+    """Print welcome message."""
+    print("Welcome to Python Training")
+
+
+if __name__ == "__main__":
+    print_welcome_message()
