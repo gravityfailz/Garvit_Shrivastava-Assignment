41919215e7e4a693a050178572282840510dafaf
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-16 16:44:16 +0530

    feat: add positive negative zero checker

--- Garvit_Shrivastava_python_training/basic_python/basics/operators.py
+++ Garvit_Shrivastava_python_training/basic_python/basics/operators.py
@@ -11,3 +11,24 @@ def check_even_or_odd(number: int) -> str:
 
 if __name__ == "__main__":
     print(check_even_or_odd(12))
+
+
+"""
+Question 8
+"""
+
+
+def identify_number_type(number: int) -> str:
+    """Identify number type."""
+
+    if number > 0:
+        return "Positive"
+
+    if number < 0:
+        return "Negative"
+
+    return "Zero"
+
+
+if __name__ == "__main__":
+    print(identify_number_type(-5))
