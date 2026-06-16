21c21e46d23e5018b3e6de8a0e9d9b4fe509628b
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-16 16:37:49 +0530

    feat: add python version checker

--- Garvit_Shrivastava_python_training/basic_python/introduction/introduction.py
+++ Garvit_Shrivastava_python_training/basic_python/introduction/introduction.py
@@ -10,3 +10,19 @@ def print_welcome_message() -> None:
 
 if __name__ == "__main__":
     print_welcome_message()
+
+
+"""
+Question 2
+"""
+
+import sys
+
+
+def display_python_version() -> None:
+    """Display installed Python version."""
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    display_python_version()
