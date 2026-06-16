f2df1d0ca52ecce160f7f05f8bf302f93d3847d8
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-16 16:46:55 +0530

    feat: print numbers from one to hundred

---
+++ Garvit_Shrivastava_python_training/basic_python/basics/loops.py
@@ -0,0 +1,14 @@
+"""
+Question 12
+"""
+
+
+def print_numbers() -> None:
+    """Print numbers from 1 to 100."""
+
+    for number in range(1, 101):
+        print(number)
+
+
+if __name__ == "__main__":
+    print_numbers()
