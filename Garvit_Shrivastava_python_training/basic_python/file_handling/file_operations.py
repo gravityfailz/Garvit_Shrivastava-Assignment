8ff1be10c558ad8d857b671e98b0937e9961d411
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-17 09:49:21 +0530

    feat: count file statistics

--- Garvit_Shrivastava_python_training/basic_python/file_handling/file_operations.py
+++ Garvit_Shrivastava_python_training/basic_python/file_handling/file_operations.py
@@ -18,3 +18,32 @@ def write_name() -> None:
 
 if __name__ == "__main__":
     write_name()
+
+"""
+Question 36
+"""
+
+FILE_NAME: str = "student.txt"
+
+
+def count_file_statistics() -> None:
+    """Count file statistics."""
+
+    with open(
+        FILE_NAME,
+        "r",
+        encoding="utf-8"
+    ) as file:
+        content: str = file.read()
+
+    words: int = len(content.split())
+    lines: int = len(content.splitlines())
+    characters: int = len(content)
+
+    print("Words:", words)
+    print("Lines:", lines)
+    print("Characters:", characters)
+
+
+if __name__ == "__main__":
+    count_file_statistics()
