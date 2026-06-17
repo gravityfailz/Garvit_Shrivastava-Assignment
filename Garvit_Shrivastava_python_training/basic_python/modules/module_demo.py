4be0333e221ebf2986c4269bef069e7f2154400c
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-17 09:42:36 +0530

    feat: create and import custom module

--- Garvit_Shrivastava_python_training/basic_python/modules/module_demo.py
+++ Garvit_Shrivastava_python_training/basic_python/modules/module_demo.py
@@ -37,3 +37,23 @@ def generate_random_numbers() -> None:
 
 if __name__ == "__main__":
     generate_random_numbers()
+
+"""
+Question 24
+"""
+
+from custom_module import (
+    add_numbers
+)
+
+
+def demonstrate_custom_module() -> None:
+    """Use custom module."""
+
+    print(
+        add_numbers(10, 20)
+    )
+
+
+if __name__ == "__main__":
+    demonstrate_custom_module()
