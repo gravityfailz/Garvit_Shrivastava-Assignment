cfb942fd112d7dbfe8ee62f6452fb678ed1f2a02
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-17 09:52:22 +0530

    feat: create car constructor example

--- Garvit_Shrivastava_python_training/python_advance/oop/models.py
+++ Garvit_Shrivastava_python_training/python_advance/oop/models.py
@@ -31,3 +31,35 @@ student = Student(
 )
 
 student.display_details()
+
+"""
+Question 41
+"""
+
+
+class Car:
+    """Car class."""
+
+    def __init__(
+        self,
+        brand: str,
+        model: str
+    ) -> None:
+        self.brand = brand
+        self.model = model
+
+    def display_car(self) -> None:
+        """Display car details."""
+
+        print(
+            self.brand,
+            self.model
+        )
+
+
+car = Car(
+    "Toyota",
+    "Innova"
+)
+
+car.display_car()
