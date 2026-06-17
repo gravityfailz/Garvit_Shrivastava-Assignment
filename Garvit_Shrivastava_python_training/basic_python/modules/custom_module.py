6a2b2d8a303fec2379eeaf4e2689c99e3a5a037c
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-17 09:41:39 +0530

    feat: add math module examples

---
+++ Garvit_Shrivastava_python_training/basic_python/modules/custom_module.py
@@ -0,0 +1,12 @@
+"""
+Custom module
+"""
+
+
+def add_numbers(
+    first_number: int,
+    second_number: int
+) -> int:
+    """Add two numbers."""
+
+    return first_number + second_number
---
+++ Garvit_Shrivastava_python_training/basic_python/modules/module_demo.py
@@ -0,0 +1,17 @@
+"""
+Question 22
+"""
+
+import math
+
+
+def math_operations() -> None:
+    """Perform math operations."""
+
+    print(math.sqrt(25))
+    print(math.pow(2, 5))
+    print(math.factorial(5))
+
+
+if __name__ == "__main__":
+    math_operations()
