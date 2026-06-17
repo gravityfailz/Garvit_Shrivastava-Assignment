9a09d33f04d4e4c1bc93240c63f850583633b13a
gravityfailz <garvitjishrivastava10@gmail.com>
2026-06-17 09:42:11 +0530

    feat: add random module examples

--- Garvit_Shrivastava_python_training/basic_python/modules/module_demo.py
+++ Garvit_Shrivastava_python_training/basic_python/modules/module_demo.py
@@ -15,3 +15,25 @@ def math_operations() -> None:
 
 if __name__ == "__main__":
     math_operations()
+
+"""
+Question 23
+"""
+
+import random
+
+
+def generate_random_numbers() -> None:
+    """Generate random values."""
+
+    print(
+        random.randint(1, 100)
+    )
+
+    print(
+        random.uniform(1, 10)
+    )
+
+
+if __name__ == "__main__":
+    generate_random_numbers()
